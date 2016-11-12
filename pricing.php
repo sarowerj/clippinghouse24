cf1bef8feb6ee462b94fc833368ab36420245047
Sarower Jahan <sarowerj@gmail.com>
2016-11-12 16:43:55 +0600

    Pricing Page created

--- pricing.php
+++ pricing.php
@@ -27,252 +27,77 @@ include('header.php');
 
 
 <!-- - - - - - - - - - SECTION - - - - - - - - - -->
-<main class="inner_page">
+<main class="inner_page pricing">
     <div class="pi-section-w pi-section-white piCounter">
         <div class="pi-section pi-padding-bottom-30">
-
-            <div class="pi-row pi-grid-small-margins">
-
-                <div class="pi-col-md-3 pi-col-xs-6">
-                    <table class="pi-pricing">
-                        <thead>
-                            <tr>
-                                <td class="pi-pricing-head-purple pi-text-center">
-                                    <span class="pi-pricing-title">Starter</span>
-
-                                    <div class="pi-pricing-price">
-                                        <span class="pi-pricing-currency">$</span>
-                                        <span class="pi-pricing-amount">29</span>
-                                        <span class="pi-pricing-period">/mo</span>
-                                    </div>
-                                    <p class="pi-italic">
-                                        Sit aspernatur aut odit aut fugit sed quia quuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>5 Website</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>10 Pages and Blogs *</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>2 GB Storage</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Custom Domain</strong> <span class="pi-text-bg-green pi-weight-700">FREE</span></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Developer Platform</strong></td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Drag and Drop Widgets</td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Email and Forum Support</td>
-                            </tr>
-                            <tr>
-                                <td class="pi-pricing-description">
-                                    <p class="pi-smaller-text">
-                                        <strong>*</strong> Voluptatem quia voluptas sit aspernatur ipsam aut odit aut fugit sed quia consequuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td>
-                                    <p>
-                                        <a href="#" class="btn pi-btn-purple pi-btn-wide">Sign Up</a>
-                                    </p>
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
+            <div class="pi-row">
+                <div class="pi-col-lg-12">
+                    <div class="item_row">
+                        <div class="item_head">
+                            <div class="big_text">Category</div>
+                            <div class="hints">This is hints for category.</div>
+                        </div>
+                        <div class="item_head">
+                            <div class="big_text">Complexity</div>
+                            <div class="hints">Complexity by time.</div>
+                        </div>
+                        <div class="item_head">
+                            <div class="big_text">Price</div>
+                            <div class="hints">Price in US Dollar.</div>
+                        </div>
+                    </div> 
+                    <div class="item_row">
+                        <div class="item_content">
+                            <span class="large">Clipping Path</span>
+                        </div>
+                        <div class="item_content">
+                            <div class="line">Simple</div>
+                            <div class="line">Medium</div>
+                            <div class="line">Complex</div>
+                            <div class="line">Extra Complex</div>
+                        </div>
+                        <div class="item_content">
+                            <div class="line">0.7</div>
+                            <div class="line">0.8</div>
+                            <div class="line"> &nbsp; </div>
+                            <div class="line">01</div>
+                        </div>
+                    </div>
+                    <div class="item_row">
+                        <div class="item_content">
+                            <span class="large">Clipping Path</span>
+                        </div>
+                        <div class="item_content">
+                            <div class="line">Simple</div>
+                            <div class="line">Medium</div>
+                            <div class="line">Complex</div>
+                            <div class="line">Extra Complex</div>
+                        </div>
+                        <div class="item_content">
+                            <div class="line">0.7</div>
+                            <div class="line">0.8</div>
+                            <div class="line"> &nbsp; </div>
+                            <div class="line">01</div>
+                        </div>
+                    </div>
+                    <div class="item_row">
+                        <div class="item_content">
+                            <span class="large">Clipping Path</span>
+                        </div>
+                        <div class="item_content">
+                            <div class="line">Simple</div>
+                            <div class="line">Medium</div>
+                            <div class="line">Complex</div>
+                            <div class="line">Extra Complex</div>
+                        </div>
+                        <div class="item_content">
+                            <div class="line">0.7</div>
+                            <div class="line">0.8</div>
+                            <div class="line"> &nbsp; </div>
+                            <div class="line">01</div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="pi-col-md-3 pi-col-xs-6">
-                    <table class="pi-pricing">
-                        <thead>
-                            <tr>
-                                <td class="pi-pricing-head-blue pi-text-center">
-                                    <span class="pi-pricing-title">Starter</span>
-
-                                    <div class="pi-pricing-price">
-                                        <span class="pi-pricing-currency">$</span>
-                                        <span class="pi-pricing-amount">29</span>
-                                        <span class="pi-pricing-period">/mo</span>
-                                    </div>
-                                    <p class="pi-italic">
-                                        Sit aspernatur aut odit aut fugit sed quia quuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>5 Website</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>10 Pages and Blogs *</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>2 GB Storage</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Custom Domain</strong> <span class="pi-text-bg-green pi-weight-700">FREE</span></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Developer Platform</strong></td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Drag and Drop Widgets</td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Email and Forum Support</td>
-                            </tr>
-                            <tr>
-                                <td class="pi-pricing-description">
-                                    <p class="pi-smaller-text">
-                                        <strong>*</strong> Voluptatem quia voluptas sit aspernatur ipsam aut odit aut fugit sed quia consequuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td>
-                                    <p>
-                                        <a href="#" class="btn pi-btn-blue pi-btn-wide">Sign Up</a>
-                                    </p>
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-
-                <div class="pi-col-md-3 pi-col-xs-6">
-                    <table class="pi-pricing">
-                        <thead>
-                            <tr>
-                                <td class="pi-pricing-head-turquoise pi-text-center">
-                                    <span class="pi-pricing-title">Starter</span>
-
-                                    <div class="pi-pricing-price">
-                                        <span class="pi-pricing-currency">$</span>
-                                        <span class="pi-pricing-amount">29</span>
-                                        <span class="pi-pricing-period">/mo</span>
-                                    </div>
-                                    <p class="pi-italic">
-                                        Sit aspernatur aut odit aut fugit sed quia quuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>5 Website</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>10 Pages and Blogs *</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>2 GB Storage</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Custom Domain</strong> <span class="pi-text-bg-green pi-weight-700">FREE</span></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Developer Platform</strong></td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Drag and Drop Widgets</td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Email and Forum Support</td>
-                            </tr>
-                            <tr>
-                                <td class="pi-pricing-description">
-                                    <p class="pi-smaller-text">
-                                        <strong>*</strong> Voluptatem quia voluptas sit aspernatur ipsam aut odit aut fugit sed quia consequuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td>
-                                    <p>
-                                        <a href="#" class="btn pi-btn-turquoise pi-btn-wide">Sign Up</a>
-                                    </p>
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-
-                <div class="pi-col-md-3 pi-col-xs-6">
-                    <table class="pi-pricing">
-                        <thead>
-                            <tr>
-                                <td class="pi-pricing-head-green pi-text-center">
-                                    <span class="pi-pricing-title">Starter</span>
-
-                                    <div class="pi-pricing-price">
-                                        <span class="pi-pricing-currency">$</span>
-                                        <span class="pi-pricing-amount">29</span>
-                                        <span class="pi-pricing-period">/mo</span>
-                                    </div>
-                                    <p class="pi-italic">
-                                        Sit aspernatur aut odit aut fugit sed quia quuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>5 Website</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>10 Pages and Blogs *</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>2 GB Storage</strong></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Custom Domain</strong> <span class="pi-text-bg-green pi-weight-700">FREE</span></td>
-                            </tr>
-                            <tr>
-                                <td><strong>Developer Platform</strong></td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Drag and Drop Widgets</td>
-                            </tr>
-                            <tr>
-                                <td><i class="icon-check pi-text-green pi-icon-left"></i>Email and Forum Support</td>
-                            </tr>
-                            <tr>
-                                <td class="pi-pricing-description">
-                                    <p class="pi-smaller-text">
-                                        <strong>*</strong> Voluptatem quia voluptas sit aspernatur ipsam aut odit aut fugit sed quia consequuntur
-                                    </p>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td>
-                                    <p>
-                                        <a href="#" class="btn pi-btn-green pi-btn-wide">Sign Up</a>
-                                    </p>
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-
             </div>
         </div>
     </div>
